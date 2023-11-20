d1851b77d524c384653b7af02a54ba67b4866d2b
gagan-gaurav <iamgagangaurav@gmail.com>
2023-11-21 01:48:48 +0530

    index.ts

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { parseInputFile, InputRecord } from './utils';
+import FareService from './fareCalculator';
+import { Trip, Line } from './types';
+
+// Create new a FareService
+const fareService = new FareService();
+
+async function processInputData() {
+	//Input file path
+	const inputFilePath = './data/fares.csv';
+
+	// Parse input data from the CSV file
+	const inputData: InputRecord[] = await parseInputFile(inputFilePath);
+
+	const userTripData: Trip[] = inputData.map(
+	({ FromLine, ToLine, DateTime }): Trip => ({
+		source: FromLine as Line,
+		destination: ToLine as Line,
+		endTime: new Date(DateTime),
+	})
+	);
+	console.log(`Total Fare : ${fareService.calculateTotalFare(userTripData)}`)
+}
+
+// Call the async function
+processInputData().catch((error) => console.error(error));
